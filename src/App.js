55b984507b692bf47ae4f3691f56de0a5bcaa23d
Michael Kroon <kroonology@gmail.com>
2019-09-19 14:40:16 -0600

    build out index.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import AccountProfile from './components/AccountProfile';
+import { Container } from 'semantic-ui-react';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-      Hello...
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Navbar />
+    <Container>
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={() => <AccountProfile />}
+        />
+      </Switch>
+    </Container>
+  </>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
